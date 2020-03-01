acc0a31872a7f9fd0d2e01b15878b7e4e8e295fe
AimbotParce <marcparcerisa@gmail.com>
2020-03-01 18:39:35 +0100

    01/03/2020

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1582669818",
+        "cacheTag": "1583071456",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1582668692,
+            "last_rendertime": 1583071190,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
